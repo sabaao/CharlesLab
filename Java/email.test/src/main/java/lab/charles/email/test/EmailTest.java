ea27416bcbd9cf4d68fdf315159dd96180c63190
charles <sabaa35@gmail.com>
2016-02-21 00:55:22 +0800

    Initial commit

---
+++ Java/email.test/src/main/java/lab/charles/email/test/EmailTest.java
@@ -0,0 +1,47 @@
+package lab.charles.email.test;
+import java.util.Properties;
+import javax.mail.*;
+import javax.mail.internet.*;
+
+public class EmailTest {
+
+	public static void main(String[] args) throws Exception{
+		// mail server connection parameters
+	    String host = "mail.yxunistar.com";
+	    String user = "charles";
+	    String password = "wf46re";
+	 
+	    // connect to my pop3 inbox
+	    Properties properties = System.getProperties();
+	    Session session = Session.getDefaultInstance(properties);
+	    Store store = session.getStore("pop3");
+	    store.connect(host, user, password);
+	    Folder inbox = store.getFolder("Inbox");
+	    inbox.open(Folder.READ_ONLY);
+	 
+	    // get the list of inbox messages
+	    Message[] messages = inbox.getMessages();
+	 
+	    if (messages.length == 0) System.out.println("No messages found.");
+	 
+	    for (int i = 0; i < messages.length; i++) {
+	      // stop after listing ten messages
+	      if (i > 10) {
+	        System.exit(0);
+	        inbox.close(true);
+	        store.close();
+	      }
+	 
+	      System.out.println("Message " + (i + 1));
+	      System.out.println("From : " + messages[i].getFrom()[0]);
+	      System.out.println("Subject : " + messages[i].getSubject());
+	      System.out.println("Sent Date : " + messages[i].getSentDate());
+	      System.out.println();
+	    }
+	 
+	    inbox.close(true);
+	    store.close();
+
+	}
+
+}
