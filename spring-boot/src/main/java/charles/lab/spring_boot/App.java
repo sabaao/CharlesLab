311bc80b2ca793a038fbad37f4fc678269684e89
charles <sabaa35@gmail.com>
2017-12-22 00:52:21 +0800

    first commit

---
+++ spring-boot/src/main/java/charles/lab/spring_boot/App.java
@@ -0,0 +1,27 @@
+package charles.lab.spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@SpringBootApplication
+@EnableDiscoveryClient
+public class App {
+	@RequestMapping(value = "/available")
+	public String available() {
+		return "Spring in Action";
+	}
+
+	@RequestMapping(value = "/checked-out")
+	public String checkedOut() {
+		return "Spring Boot in Action";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+}
