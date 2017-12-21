d818fa2439770ce2ebbd8327e03b32f20bfb92cb
charles <sabaa35@gmail.com>
2017-12-22 00:51:05 +0800

    add filter

--- zuul/src/main/java/charles/lab/zuul/App.java
+++ zuul/src/main/java/charles/lab/zuul/App.java
@@ -4,14 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @EnableDiscoveryClient
 @EnableZuulProxy
 @SpringBootApplication
-public class App 
-{
-    public static void main( String[] args )
-    {
-    	SpringApplication.run(App.class, args);
-    }
+public class App {
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+	
+	@Bean
+	public SimpleFilter simpleFilter() {
+		return new SimpleFilter();
+	}
 }
