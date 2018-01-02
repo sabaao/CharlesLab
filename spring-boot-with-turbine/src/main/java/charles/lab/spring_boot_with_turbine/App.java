95e19eeb7437fc7cfa7dbc22d4ffbc928f98255f
charles <sabaa35@gmail.com>
2018-01-03 00:43:30 +0800

    first commit

---
+++ spring-boot-with-turbine/src/main/java/charles/lab/spring_boot_with_turbine/App.java
@@ -0,0 +1,19 @@
+package charles.lab.spring_boot_with_turbine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableHystrixDashboard
+@EnableTurbine
+public class App {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+}
