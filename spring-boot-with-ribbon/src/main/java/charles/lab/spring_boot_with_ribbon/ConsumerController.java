d9df370f9eb36180bea6293b60076c97e80f8bb6
charles <sabaa35@gmail.com>
2018-01-30 23:49:05 +0800

    add swagger

--- spring-boot-with-ribbon/src/main/java/charles/lab/spring_boot_with_ribbon/ConsumerController.java
+++ spring-boot-with-ribbon/src/main/java/charles/lab/spring_boot_with_ribbon/ConsumerController.java
@@ -2,6 +2,8 @@ package charles.lab.spring_boot_with_ribbon;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -11,7 +13,7 @@ public class ConsumerController {
     @Autowired
     private RestTemplate restTemplate;
 
-    @GetMapping(value = "/bookavailable")
+    @RequestMapping(value = "/bookavailable", method =RequestMethod.GET )
     public String getBookAvailable() {
         return restTemplate.getForEntity("http://book/available", String.class).getBody();
     }
