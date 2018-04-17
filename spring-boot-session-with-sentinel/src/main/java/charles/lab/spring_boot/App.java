a2d8c87cd390a3cd4da9d878564ff42aad146c9a
charles <charles@cathayholdings.com.tw>
2018-04-17 17:09:58 +0800

    first commit

---
+++ spring-boot-session-with-sentinel/src/main/java/charles/lab/spring_boot/App.java
@@ -0,0 +1,12 @@
+package charles.lab.spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class App {
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+}
---
+++ spring-boot-session-with-sentinel/src/main/java/charles/lab/spring_boot/HttpSessionConfig.java
@@ -0,0 +1,10 @@
+package charles.lab.spring_boot;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+@Configuration
+@EnableRedisHttpSession
+public class HttpSessionConfig {
+
+}
