10f3f97ccd980200db58afdba26181a5803eb42a
charles <sabaa35@gmail.com>
2018-06-08 10:12:14 +0800

    add eureka client

--- spring-boot-with-actuator/src/main/java/charles/lab/spring_boot_with_actuator/App.java
+++ spring-boot-with-actuator/src/main/java/charles/lab/spring_boot_with_actuator/App.java
@@ -3,12 +3,14 @@ package charles.lab.spring_boot_with_actuator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 @SpringBootApplication
+@EnableDiscoveryClient
 public class App {
 	@RequestMapping(value = "/available")
 	public String available() {
