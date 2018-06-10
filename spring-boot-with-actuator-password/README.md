318f09fec875f62938321796b63a7f07e6da00b0
charles <sabaa35@gmail.com>
2018-06-10 12:19:32 +0800

    first commit

---
+++ spring-boot-with-actuator-password/README.md
@@ -0,0 +1,12 @@
+This actuator include spring security, you need use basic auth for check username/password.
+
+Username is admin, password is secret, you can find at applicaation.yml.
+
+Try it from http://localhost:11111/manage/health, and you need input username/password.
+
+You also can put username/password in url, like this,
+http://admin:secret@localhost:11111/manage/health
+
+
+Reference:
+https://www.jianshu.com/p/b0b40038bb93
---
+++ spring-boot-with-actuator-password/src/main/java/charles/lab/spring_boot_with_actuator/App.java
@@ -0,0 +1,27 @@
+package charles.lab.spring_boot_with_actuator;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@SpringBootApplication
+
+public class App {
+	@RequestMapping(value = "/available")
+	public String available() {
+		return "Spring in Action";
+	}
+
+	@RequestMapping(value = "/checked-out")
+	public String checkedOut() {
+		return "Spring Boot in Action";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+}
